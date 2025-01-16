e742f084fc0b1c7a40d160da4d1faa0a19521d57
wbeimar <wbeimarmonsalve33@gmail.com>
2025-01-15 20:11:03 -0500

    Concatenando los nombres del array

--- forEach.js
+++ forEach.js
@@ -4,13 +4,18 @@
 // Aquí haces algo con 'elemento'
 // });
 
-// Ejercicio: Filtrar números pares: Escribe un programa que recorra un array 
-// de números y use forEach para imprimir solo los números pares.
+// Ejercicio: Concatenar nombres: Dado un array de nombres, usa forEach para 
+//concatenar todos los nombres en una sola cadena, separados por comas, y luego imprime 
+// la cadena resultante.
 
-let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const nombres = ['Ana', 'Carlos', 'Beatriz', 'David', 'Elena'];
+let cadenaConcatenada = '';
 
-numeros.forEach(function(numero) {
-    if (numero % 2 === 0) {
-        console.log(numero);
+nombres.forEach(function(nombre, indice) {
+    cadenaConcatenada += nombre;
+    if (indice < nombres.length - 1) {
+        cadenaConcatenada += ', ';
     }
 });
+
+console.log('Nombres concatenados:', cadenaConcatenada);
