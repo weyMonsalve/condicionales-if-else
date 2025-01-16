3654e5d7ef80529d5d3cb57ea8a179b60da31d69
wbeimar <wbeimarmonsalve33@gmail.com>
2025-01-15 19:57:48 -0500

    Convertir a mayusculas

--- forEach.js
+++ forEach.js
@@ -4,11 +4,11 @@
 // Aquí haces algo con 'elemento'
 // });
 
-// Ejercicio: Imprimir nombres con índice: Dado un array de nombres, utiliza 
-// forEach para imprimir cada nombre junto con su posición en el array (índice).
+// Ejercicio: Convertir a mayúsculas: Crea un programa que tome un array de 
+// cadenas de texto y use forEach para imprimir cada cadena en mayúsculas.
 
-let nombres = ["Andres", "Julio", "mauricio", "hector"];
+let palabras = ["hola", "casa", "carro", "futbol", "comida"];
 
-nombres.forEach(function(nombre, indice) {
-    console.log("nombre: " + nombre, "indice: " + indice);
+palabras.forEach(function(cadena) {
+    console.log(cadena.toUpperCase());
 });
